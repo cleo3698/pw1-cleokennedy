6fdedb85f6a50ac19a74e8e38f81456fab6cd780
cleo3698 <salgado.cfk@gmail.com>
2024-12-02 20:17:12 -0300

    exercicios_javascript

---
+++ exercicios_js/exe_2.js
@@ -0,0 +1,18 @@
+
+function verificarNumero(numero){
+  
+  if (numero % 2 === 0){
+    console.log("O numero é PAR.");
+  } else {
+    console.log("O numero é IMPAR.");
+  }
+
+  if(numero> 0){
+    console.log("o numero é POSITIVO.");
+  } else if(numero <0){
+    console.log("O numero é Negativo");
+  } else {
+    console.log("O numero é ZERO");
+  }
+}
+
